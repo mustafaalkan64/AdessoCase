3ad18cfcbf1109bfb3f9c56614a2b90eab424b39
Mustafa Alkan <mustafa.alkan@eczacibasi.com.tr>
2023-08-25 16:58:44 +0300

    Travel Dto validator refactored

--- AdessoCase.Service/Validations/TravelDtoValidator.cs
+++ AdessoCase.Service/Validations/TravelDtoValidator.cs
@@ -9,6 +9,9 @@ namespace AdessoCase.Service.Validations
         {
 
             RuleFor(x => x.TravelDate).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.SeatCount).GreaterThan(0).WithMessage("Seat Count Should be Greater Then 0");
+            RuleFor(x => x.ArrivalCityId).GreaterThan(0).WithMessage("Arrive City Id Should be Greater Then 0");
+            RuleFor(x => x.DepartureCityId).GreaterThan(0).WithMessage("Departure City Id Should be Greater Then 0");
             RuleFor(x => x.ArrivalCityId).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
             RuleFor(x => x.DepartureCityId).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
             
